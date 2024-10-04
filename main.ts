02fb9ae1a502212530bcbd4f045552dcdd9de7e6
maryam-ergen <maryam.ergen@mths.ca>
2024-10-04 10:54:52 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,28 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
+ * Created by: Maryam Ergen
  * Created on: Oct 2024
  * This program blinks with color
 */
 
 basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
+    // blue
     pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
     basic.showString ("Blue")
     pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
     pause(1)
+
+    // green
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString("Green")
     pins.digitalWritePin(DigitalPin.P14, 0)
